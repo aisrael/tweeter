376440ca14b3eff49e06c25a729912b037b5b87c
Alistair A. Israel <aisrael@gmail.com>
2018-11-27 10:56:35 +0800

    Now displays timestamp (in local timezone)

--- lib/tweeter_web/controllers/page_controller.ex
+++ lib/tweeter_web/controllers/page_controller.ex
@@ -5,4 +5,10 @@ defmodule TweeterWeb.PageController do
     [ tweet | _ ] =Tweeter.Repo.all(Tweeter.Tweet)
     render(conn, "tweets.html", tweet: tweet, layout: {TweeterWeb.LayoutView, "tweeter.html"})
   end
+
+  def format_timestamp(tweet) do
+    tweet.inserted_at |>
+    Timex.Timezone.convert(Timex.Timezone.local) |>
+    Timex.format!("{M}/{D}/{YYYY} {h24}:{m}{am}")
+  end
 end
