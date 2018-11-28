2af734f2a94c8583c12abbbcfce922a53e346938
Alistair A. Israel <aisrael@gmail.com>
2018-11-28 09:24:22 +0800

    Now displays a list of tweets

--- lib/tweeter_web/controllers/page_controller.ex
+++ lib/tweeter_web/controllers/page_controller.ex
@@ -2,8 +2,8 @@ defmodule TweeterWeb.PageController do
   use TweeterWeb, :controller
 
   def index(conn, _params) do
-    [ tweet | _ ] =Tweeter.Repo.all(Tweeter.Tweet)
-    render(conn, "tweets.html", tweet: tweet, layout: {TweeterWeb.LayoutView, "tweeter.html"})
+    tweets = Tweeter.Repo.all(Tweeter.Tweet)
+    render(conn, "tweets.html", tweets: tweets, layout: {TweeterWeb.LayoutView, "tweeter.html"})
   end
 
   def format_timestamp(tweet) do
